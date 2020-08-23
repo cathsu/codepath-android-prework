de9eb09d2ecc519c3d2ce02ea3e8be40f595d7f9
Cathy Hsu <chsucathy@gmail.com>
2020-08-22 22:34:45 -0700

    Render items

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+
+        // items is the data source to be populated in rvItems, the RecylerView.
+        // itemsAdapter is responsible for converting each element in items into an row item in rvItems.
+        // to retrieve the views in each row item in rvItems, we create a ViewHolder within the itemsAdapter class
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }
